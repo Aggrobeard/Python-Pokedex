26627ab5a50224732c3e55ca91dea5a184542c1c
Aggrobeard <31164544+Aggrobeard@users.noreply.github.com>
2022-06-02 09:56:52 -0400

    Add files via upload

--- ImageDownloader.py
+++ ImageDownloader.py
@@ -1,7 +1,5 @@
 import urllib.request
-import PIL.Image
-import os
-import PokeParser
+
 
 """ASCII_CHARS = ['@', '#', 'S', '%', '?', '*', '+', ';', ':', ',', '.']"""
 ASCII_CHARS = [' ', '.', ',', ':', ';', '+', '*', '?', '%', 'S', '#', '@']
@@ -30,19 +28,3 @@ def dl_png(url, file_path):
     file_name = 'pokepic'
     full_path = (file_path + file_name + '.png')
     urllib.request.urlretrieve(url, full_path)
-
-
-def create_art(file_name):
-    url = PokeParser.poke_list[4]
-    dl_png(url, 'downloads/' + file_name)
-    path = 'downloads/pokepicpokepic.png'
-    image = PIL.Image.open(path)
-
-    new_image_data = pixels_to_ascii(grayify(resize_image(image)))
-    new_width = 100
-    pixel_count = len(new_image_data)
-    ascii_image = "\n".join(new_image_data[i:(i+new_width)] for i in range(0, pixel_count, new_width))
-
-    print(ascii_image)
-
-    os.remove('downloads/pokepicpokepic.png')
